295f44445b7b175d42ea69f9fb518b1285f3259c
yogesh jadhav <yogesh131jadhav@gmail.com>
2020-01-08 00:56:12 +0530

    Removed all project except SK and inventory

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
-angular.module('skWholesaler', [])
+angular.module('skWholesale', [])
 .controller('MainCtrl', function($scope, $http, $timeout, $window) {
   $scope.activePage = 1;
   $scope.singleProductDetail = null;
   $scope.showPage = false;
+  $scope.title = 'Product List';
   $http.get("./stub/stub.json").then(function (response) {
     $timeout(() => {
       $scope.showPage = true;
@@ -14,6 +15,7 @@ angular.module('skWholesaler', [])
       top: 0,
       behavior: 'smooth',
     });
+    $scope.title = 'Product List';
     return menu === 'Home' ? $scope.activePage = 1 : $scope.activePage = 2;
   }
   $scope.productDetailsPage = function(product) {
@@ -21,7 +23,23 @@ angular.module('skWholesaler', [])
       top: 0,
       behavior: 'smooth',
     });
+    $scope.title = $scope.singleProductDetail.title;
     $scope.activePage = 3;
     $scope.singleProductDetail = product;
   }
+  $scope.aboutUsPage = function() {
+    $window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+    $scope.title = 'About Us';
+    $scope.activePage = 4;
+  }
 });
+
+/*
+ * 1: Home page
+ * 2: Product List page
+ * 3: Product Details page
+ * 4: About Us page
+ */
