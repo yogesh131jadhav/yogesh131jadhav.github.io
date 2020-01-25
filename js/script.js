076b9d70171796568a43b32ef0f31934776c42c7
yogesh jadhav <yogesh131jadhav@gmail.com>
2020-01-26 01:18:43 +0530

    Branding page related changes

--- js/script.js
+++ js/script.js
@@ -5,6 +5,8 @@ angular.module('skWholesale', [])
   $scope.showPage = false;
   $scope.mobileNavClass = false;
   $scope.title = 'Product List';
+  $scope.selectedBrand = null;
+  $scope.selectedBrandBreadCrumb = [];
   $http.get("./stub/stub.json").then(function (response) {
     $timeout(() => {
       $scope.showPage = true;
@@ -48,6 +50,29 @@ angular.module('skWholesale', [])
   }
   $scope.handleMobileNav = function() {
     $scope.mobileNavClass = $scope.mobileNavClass ? false : true;
+  }
+  $scope.toggleBrandNavClass = function(brand, index) {
+    $scope.handleBreadCrumbArray(brand, index);
+    if(brand.subItem && brand.subItem.length && brand.subItem[0].subItem && brand.subItem[0].subItem.length) {
+      brand.toggleClass = brand.toggleClass ? false : true;
+    } else {
+      $scope.setSelectedBrand(brand);
+    }
+  }
+  $scope.setSelectedBrand = function(setBrandObject) {
+    $scope.handleBreadCrumbArray(setBrandObject, 2);
+    $scope.selectedBrand = setBrandObject;
+  }
+  $scope.handleBreadCrumbArray = function(brand, index) {
+    $scope.selectedBrandBreadCrumb.splice(index + 1);
+    // $scope.selectedBrandBreadCrumb.push({"breadCrumb": brand.title});
+    if($scope.selectedBrandBreadCrumb.indexOf(brand.title) === -1) {
+      if($scope.selectedBrandBreadCrumb[index]) {
+        $scope.selectedBrandBreadCrumb[index] = brand.title;
+      } else {
+        $scope.selectedBrandBreadCrumb.push(brand.title);
+      }
+    }
   }
 });
 
