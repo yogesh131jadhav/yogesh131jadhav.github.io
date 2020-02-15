6c59d32452215ce0d1e9b8084d6de49951f2fbf2
yogesh jadhav <yogesh131jadhav@gmail.com>
2020-02-15 16:40:39 +0530

    Updated github page with angular project

--- inventory/component/suppliers/suppliers.component.js
+++
@@ -1,9 +0,0 @@
-angular.module('inventoryManagement')
-.component('suppliersComponent', {
-  templateUrl: './component/suppliers/suppliers.component.html',
-  bindings: {
-     inventoryManagementObject: '<'
-  },
-  controller: function() {
-  }
-});
