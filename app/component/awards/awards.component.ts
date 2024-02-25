c3cfc79334ed2f4afd17505f078ec6222fb67725
yogesh131jadhav@gmail.com <yogesh131jadhav@gmail.com>
2024-02-25 23:46:06 +0530

    New website new look

---
+++ app/component/awards/awards.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-awards',
+  standalone: true,
+  imports: [],
+  templateUrl: './awards.component.html',
+  styleUrl: './awards.component.scss'
+})
+export class AwardsComponent {
+
+}
---
+++ app/component/nav/nav.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.scss'
+})
+
+export class NavComponent {
+
+}
